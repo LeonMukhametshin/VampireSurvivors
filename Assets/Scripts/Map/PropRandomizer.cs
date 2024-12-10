6ee618b8faecb7e25e381d78bda20e8a1b2b66cb
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2024-12-10 12:37:44 +0300

    fix: изменение ошибок генерации и переподключение объектов

--- Assets/Scripts/Map/PropRandomizer.cs
+++ Assets/Scripts/Map/PropRandomizer.cs
@@ -8,16 +8,19 @@ public class PropRandomizer : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log("Start Prop");
         SpawnProps();
     }
 
     private void SpawnProps()
     {
+        Debug.Log("Start Spawn Prop");
         foreach (var spawnPoint in propSpawnPoints)
         {
             int random = Random.Range(0, propSpawnPoints.Count);
             GameObject prop = Instantiate(propPrefabs[random], spawnPoint.transform.position, Quaternion.identity);
             prop.transform.parent = spawnPoint.transform;
         }
+        Debug.Log("Finish Prop spawn");
     }
 }
